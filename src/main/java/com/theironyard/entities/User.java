50929a2e7f9be766b691007b62256a864efe9f08
benjamindrake <spcbdrake@yahoo.com>
2015-11-17 12:14:33 -0500

    IRONGRAM

---
+++ src/main/java/com/theironyard/entities/User.java
@@ -0,0 +1,22 @@
+package com.theironyard.entities;
+
+
+import javax.persistence.*;
+
+/**
+ * Created by benjamindrake on 11/17/15.
+ */
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue
+    @Column(nullable= false)
+    public int id;
+
+    @Column(nullable = false)
+    public String username;
+
+    @Column(nullable = false)
+    public String password;
+}
