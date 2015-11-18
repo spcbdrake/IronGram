60974677873b027c52c8954b752faceb6825d74d
benjamindrake <spcbdrake@yahoo.com>
2015-11-18 09:21:59 -0500

    fix stuff

--- src/main/java/com/theironyard/controllers/IronGramController.java
+++ src/main/java/com/theironyard/controllers/IronGramController.java
@@ -80,7 +80,11 @@ public class IronGramController {
             throw new Exception("Receiver name doesn't exist");
         }
 
-        File photoFile = File.createTempFile("photo", ".jpg", new File("public"));
+        if (!photo.getContentType().startsWith("image/")) {
+            throw new Exception ("Only images are allowed");
+        }
+
+        File photoFile = File.createTempFile("photo", photo.getOriginalFilename(), new File("public"));
         FileOutputStream fos = new FileOutputStream(photoFile);
         fos.write(photo.getBytes());
 
